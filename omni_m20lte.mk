e1207ca745cd40f6c585cfff047729c75d44b271
eun <gianpaoloestacio5@gmail.com>
2021-11-20 08:59:00 +0800

    m20lte: PBRP Bringup!

--- omni_m20lte.mk
+++ omni_m20lte.mk
@@ -20,7 +20,7 @@ PRODUCT_RELEASE_NAME := m20lte
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_DEVICE := m20lte
 PRODUCT_NAME := omni_m20lte
